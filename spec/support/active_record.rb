18608c55ebbf2519e12dd6c106e65ee4ecf202b9
Sven Fuchs <svenfuchs@artweb-design.de>
2011-11-07 13:28:38 +0100

    make active record connection setup platform agnostic

--- spec/support/active_record.rb
+++ spec/support/active_record.rb
@@ -7,13 +7,14 @@ require 'support/factories'
 
 FileUtils.mkdir_p('log')
 
+config = if RUBY_PLATFORM == 'java'
+  { 'adapter' => 'jdbcpostgresql', 'database' => 'travis_test', 'username' => ENV['USER'], 'encoding' => 'unicode' }
+else
+  { 'adapter' => 'postgresql', 'database' => 'travis_test', 'encoding' => 'unicode' }
+end
+
 ActiveRecord::Base.logger = Logger.new('log/test.db.log')
-ActiveRecord::Base.configurations = { 'test' => {
-  'adapter' => 'jdbcpostgresql',
-  'database' => 'travis_test',
-  'username' => ENV['USER'],
-  'encoding' => 'unicode'
-} }
+ActiveRecord::Base.configurations = { 'test' => config }
 ActiveRecord::Base.establish_connection('test')
 
 DatabaseCleaner.strategy = :truncation
