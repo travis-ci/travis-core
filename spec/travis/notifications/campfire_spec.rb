7e6b9cf483428bdd803b09c043536ab512456c14
Damien Mathieu <42@dmathieu.com>
2011-11-26 11:42:15 +0100

    implement the campfire notification
    Exported from the (stale) feature/campfire branch on travis-ci
    https://github.com/travis-ci/travis-ci/commit/47e462ccc68a0b69e80ae1c5f636f1a098ae072e
    
    Note : there is no encryption of the campfire token (yet).
    Putting your campfire token publicly in your repository is not safe.
    Please wait until we implement it before using this notifier.

---
+++ spec/travis/notifications/campfire_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe Travis::Notifications::Campfire do
+
+  before do
+    Travis.config.notifications = [:campfire]
+    stub_http
+  end
+
+  let(:dispatch) { lambda { |event, object| Travis::Notifications.dispatch(event, object) } }
+
+  it "sends campfire notifications to the rooms given as an array" do
+    targets = ['evome:apitoken@42', 'rails:sometoken@69']
+    build = Factory(:build, :config => { 'notifications' => { 'campfire' => targets } })
+    dispatch.should post_campfire_on('build:finished', build, :to => targets)
+  end
+
+  it "sends campfire notifications to the room given as a string" do
+    target = 'evome:apitoken@42'
+    build = Factory(:build, :config => { 'notifications' => { 'campfire' => target } })
+    dispatch.should post_campfire_on('build:finished', build, :to => [target])
+  end
+
+  it "sends no campfire notification if the given url is blank" do
+    build = Factory(:build, :config => { 'notifications' => { 'campfire' => '' } })
+    # No need to assert anything here as Faraday would complain about a request not being stubbed <3
+    dispatch.call('build:finished', build)
+  end
+
+
+  def stub_http
+    $http_stub ||= Faraday::Adapter::Test::Stubs.new
+    Travis::Notifications::Webhook.http_client = Faraday.new do |f|
+      f.request :url_encoded
+      f.adapter :test, $http_stub
+    end
+  end
+end
