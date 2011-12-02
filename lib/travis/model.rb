ea8c4af86dcf430e4208b965f63d11ce6519b80a
Sven Fuchs <svenfuchs@artweb-design.de>
2011-12-02 21:23:52 +0100

    autoload activerecord models on Travis::Model so preload_constants can pick them up

--- lib/travis/model.rb
+++ lib/travis/model.rb
@@ -1,26 +1,13 @@
-require 'active_support/core_ext/string/inflections'
-require 'active_support/core_ext/module/delegation'
-
 module Travis
   class Model
     autoload :Artifact,    'travis/model/artifact'
     autoload :Build,       'travis/model/build'
     autoload :Job,         'travis/model/job'
+    autoload :Repository,  'travis/model/repository'
     autoload :Request,     'travis/model/request'
     autoload :ServiceHook, 'travis/model/service_hook'
-
-    class << self
-      def find(id)
-        new(self.class.name.demodulize.constantize.find(id))
-      end
-    end
-
-    delegate :save!, :to => :record
-
-    attr_reader :record
-
-    def initialize(record)
-      @record = record
-    end
+    autoload :Token,       'travis/model/token'
+    autoload :User,        'travis/model/user'
+    autoload :Worker,      'travis/model/worker'
   end
 end
