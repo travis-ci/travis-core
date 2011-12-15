cb10ae2e2fbc37b305bfefc6d56e70a90ed7f8ec
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2011-12-15 22:51:54 +0100

    Add specs for GithubApi.remove_service_hook

--- spec/travis/github_api_spec.rb
+++ spec/travis/github_api_spec.rb
@@ -33,4 +33,27 @@ describe Travis::GithubApi do
 
   end
 
+  describe ".remove_service_hook" do
+
+    it "should remove a service hook" do
+      client_stub = stub.tap do |client|
+        client.expects(:unsubscribe_service_hook).with('owner/name', 'Travis')
+      end
+
+      Octokit::Client.stubs(:new).
+        with(:oauth_token => 't0k3n').
+        returns(client_stub)
+
+      Travis::GithubApi.remove_service_hook('owner', 'name', 't0k3n')
+    end
+
+    it "should raise an error when an unprocessable entity error occurs" do
+      Octokit::Client.stubs(:new).raises(Octokit::UnprocessableEntity)
+      expect {
+        Travis::GithubApi.remove_service_hook('owner', 'name', 't0k3n')
+      }.to raise_error Travis::GithubApi::ServiceHookError, 'error unsubscribing from the GitHub push event'
+    end
+
+  end
+
 end
