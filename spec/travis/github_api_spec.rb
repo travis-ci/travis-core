b00fcbd0b39a6f17f5c5e4573fc139ba6bad9b1d
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2011-12-15 21:05:27 +0100

    Move the dots in the Github API spec, for JRuby

--- spec/travis/github_api_spec.rb
+++ spec/travis/github_api_spec.rb
@@ -10,15 +10,14 @@ describe Travis::GithubApi do
         client.expects(:subscribe_service_hook).with('owner/name', 'Travis', {})
       end
 
-      Octokit::Client.stubs(:new)
-        .with(:oauth_token => 't0k3n')
-        .returns(client_stub)
+      Octokit::Client.stubs(:new).
+        with(:oauth_token => 't0k3n').
+        returns(client_stub)
 
       Travis::GithubApi.add_service_hook('owner', 'name', 't0k3n', {})
     end
 
     pending "should raise an error when a service hook error occurs" do
-
     end
 
     pending "should raise an error when an unauthorized error occurs" do
