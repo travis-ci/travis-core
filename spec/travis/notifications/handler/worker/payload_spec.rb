b47b45654a0925d4437866f58d58b763c763193e
Sven Fuchs <svenfuchs@artweb-design.de>
2012-02-21 00:43:53 +0100

    add minimal worker payload specs

--- spec/travis/notifications/handler/worker/payload_spec.rb
+++ spec/travis/notifications/handler/worker/payload_spec.rb
@@ -2,5 +2,18 @@ require 'spec_helper'
 require 'support/active_record'
 
 describe Travis::Notifications::Handler::Worker::Payload do
-  it 'should be specified'
+  Payload = Travis::Notifications::Handler::Worker::Payload
+
+  let(:configure) { Factory(:configure) }
+  let(:test)      { Factory(:test) }
+
+  describe 'for returns the payload required for worker jobs' do
+    it 'Job::Configure' do
+      Payload.for(configure).keys.should == [:build, :repository, :queue]
+    end
+
+    it 'Job::Test' do
+      Payload.for(test).keys.should == [:build, :repository, :config, :queue]
+    end
+  end
 end
