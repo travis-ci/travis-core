1e007fb8196412867978383c2315f703151f357d
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2012-03-02 15:49:16 +0100

    lazy generate non-primitive attributes for github objects

--- lib/travis/github.rb
+++ lib/travis/github.rb
@@ -53,52 +53,73 @@ module Travis
         end
       end
 
+      def api_url?(url)
+        url.start_with? "/" or url.start_with? api_host
+      end
+
       def get(url, &block)
         notify("get.github", url) { connection.get(url, headers, &block) }.body
       end
     end
 
     class Resource
-      attr_accessor :url, :loaded, :session
+      attr_accessor :url, :api_url, :loaded, :session, :_links
 
       alias public_send send unless method_defined? :public_send
       alias loaded? loaded
 
-      def self.attribute(*names)
+      def self.attribute(*names, &block)
         names.each do |name|
           name = name.to_s
           define_method("#{name}=") do |value|
-            @attributes[name] = value
+            attributes.delete name
+            raw_attributes[name] = value
           end
 
-          define_method(name) do
-            load unless @attributes.include? name
-            @attributes[name]
+          define_method(name) do |*a|
+            public_send("#{name}=", *a) if a.size > 0
+            @current_method = name
+            load unless attributes.include? name or raw_attributes.include? name
+            attributes[name] ||= begin
+              value = raw_attributes[name]
+              value = instance_exec(value, &block) if block and value
+              value
+            end
           end
         end
       end
 
-      def self.new(obj)
+      def self.new(obj, *)
         self === obj ? obj : super
       end
 
       def initialize(options = {})
-        @loaded     = false
-        @attributes = {}
-        set(options)
+        @loaded = false
+        set options
       end
 
       def session
         @session || Travis::Github.session
       end
 
-      def set(options, ignore_missing = false)
-        options.each_pair { |k, v| public_send "#{k}=", v if respond_to? "#{k}=" or !ignore_missing }
+      def url=(value)
+        self.api_url = value unless value.start_with? "https:\/\/github.com"
+        @url = value
+      end
+
+      def api_url
+        @api_url ||= default_url
+      end
+
+      def set(options)
+        raise ArgumentError, "no options given" unless options
+        options.each_pair { |k, v| raw_attributes[k.to_s] = v }
+        options.each_pair { |k, v| public_send "#{k}=", v if respond_to? "#{k}=" }
       end
 
       def payload=(raw)
         raw = ActiveSupport::JSON.decode(raw.to_str) if raw.respond_to? :to_str
-        set raw, true
+        set raw
       end
 
       def load
