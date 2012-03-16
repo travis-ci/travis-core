67288d3520efe613dd7a3c82ed6f27a9065083c7
Josh Kalderimis <josh.kalderimis@gmail.com>
2012-03-16 19:29:23 +0100

    pushing a broken spec for @mattmatt to help with :)

--- spec/travis/model/build_spec.rb
+++ spec/travis/model/build_spec.rb
@@ -104,6 +104,12 @@ describe Build do
         build = Factory(:build, :config => { 'foo' => { 'bar' => 'bar' } })
         build.config[:foo][:bar].should == 'bar'
       end
+
+      fit 'tries to deserialize the config itself if a String is returned' do
+        build = Factory(:build)
+        build.expects(:read_attribute).with('config').returns("---\nfoo:\n  bar: bar")
+        build.config[:foo][:bar].should == 'bar'
+      end
     end
 
     it 'sets its number to the next build number on creation' do
