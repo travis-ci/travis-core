53f522df68950a3381f5664b51512679dfb3abd6
Josh Kalderimis <josh.kalderimis@gmail.com>
2012-04-07 16:14:16 +0200

    don't be too velous when escaping and gsubbing the queue

--- lib/travis/model/repository.rb
+++ lib/travis/model/repository.rb
@@ -51,8 +51,8 @@ class Repository < ActiveRecord::Base
     end
 
     def search(query)
-      query = "%#{query.gsub('\\', '\/')}%"
-      where("(repositories.owner_name || chr(47) || repositories.name) ILIKE ?", query)
+      query = query.gsub('\\', '/')
+      where("(repositories.owner_name || chr(47) || repositories.name) ILIKE ?", "%#{query}%")
     end
 
     def find_by(params)
