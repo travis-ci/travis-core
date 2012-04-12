7a6563c68fcbecfc20c01351c188b7287fea0b29
Sven Fuchs <svenfuchs@artweb-design.de>
2012-04-13 01:19:39 +0200

    extract request creation to a factory

--- lib/travis/model/request.rb
+++ lib/travis/model/request.rb
@@ -9,37 +9,16 @@ require 'metriks'
 class Request < ActiveRecord::Base
   autoload :Approval, 'travis/model/request/approval'
   autoload :Branches, 'travis/model/request/branches'
+  autoload :Factory,  'travis/model/request/factory'
   autoload :Payload,  'travis/model/request/payload'
   autoload :States,   'travis/model/request/states'
 
   include Approval, States
 
   class << self
-    # TODO clean this up, maybe extract a factory?
     def create_from(type, data, token)
       ActiveSupport::Notifications.publish('github.requests', 'received', data)
-      payload = Travis::Github::Payload.for(type, data)
-      if payload.accept?
-        owner = owner_for(payload.owner)
-        repository = repository_for(payload.repository, owner)
-        commit = commit_for(payload.commit, repository) if payload.commit
-        repository.requests.create!(payload.request.merge(:state => :created, :commit => commit, :owner => owner, :token => token, :event_type => type))
-      end
-    end
-
-    def owner_for(attrs)
-      type = attrs[:type].constantize
-      type.find_by_login(attrs[:login]) || type.create_from_github(attrs[:login])
-    end
-
-    def repository_for(attrs, owner)
-      Repository.find_or_create_by_owner_name_and_name(owner.login, attrs[:name]).tap do |repo|
-        repo.update_attributes! attrs.merge(:owner => owner)
-      end
-    end
-
-    def commit_for(attrs, repository)
-      Commit.create!(attrs.merge(:repository_id => repository.id))
+      Factory.new(type, data, token).request
     end
   end
 
---
+++ lib/travis/model/request/factory.rb
@@ -0,0 +1,48 @@
+class Request
+  class Factory
+    attr_reader :type, :data, :token
+
+    def initialize(type, data, token)
+      @type = type
+      @data = data
+      @token = token
+    end
+
+    def request
+      if payload.accept?
+        data = payload.request
+        data.merge!(:state => :created, :commit => commit, :owner => owner, :token => token, :event_type => type)
+        repository.requests.create!(data)
+      end
+    end
+
+    private
+
+      def payload
+        @payload ||= Travis::Github::Payload.for(type, data)
+      end
+
+      def owner
+        @owner ||= begin
+          data = payload.owner
+          type = data[:type].constantize
+          type.find_by_login(data[:login]) || type.create_from_github(data[:login])
+        end
+      end
+
+      def repository
+        @repository ||= begin
+          data = payload.repository
+          Repository.find_or_create_by_owner_name_and_name(owner.login, data[:name]).tap do |repo|
+            repo.update_attributes! data.merge(:owner => owner)
+          end
+         end
+      end
+
+      def commit
+        @commit ||= if data = payload.commit
+          Commit.create!(data.merge(:repository_id => repository.id))
+        end
+      end
+  end
+end
