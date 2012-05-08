8c5c2878c318e6940f4608921ba0dfb35d81f5d5
Mathias Meyer <meyer@paperplanes.de>
2012-05-08 19:23:43 +0200

    WIP: Make instrumentation functional again, track metrics.

--- lib/travis/notifications/instrumentation.rb
+++ lib/travis/notifications/instrumentation.rb
@@ -1,4 +1,6 @@
 require 'active_support/notifications'
+require 'active_support/core_ext/string'
+require 'metriks'
 
 module Travis
   module Notifications
@@ -10,11 +12,8 @@ module Travis
         include Travis::Logging
 
         def log_notification(channel, started_at, finished_at, hash, args)
-          target, args = args.values_at(:target, :args)
-          event = args.shift
-          args = args.map { |arg| arg.is_a?(ActiveRecord::Base) ? "#<#{arg.class.name} id: #{arg.id}>" : arg.inspect }
-          message = "#{event} (#{finished_at - started_at}) #{args.join(', ')}"
-          info(colorize(:green, message), :header => channel)
+          time = finished_at - started_at
+          Metriks.timer(channel).update(time)
         end
       end
 
@@ -25,11 +24,12 @@ module Travis
       end
 
       %w(archive email github irc pusher webhook).each do |name| # TODO where can we hook in best?
-        ActiveSupport::Notifications.subscribe(name.to_s, &method(:log_notification))
+        ActiveSupport::Notifications.subscribe("#{name.to_s}.notifications.travis", &method(:log_notification))
       end
 
-      def instrument(*args, &block)
-        ActiveSupport::Notifications.instrument(name.to_s, :target => self, :args => args, &block)
+      def instrument(event, *args, &block)
+        instrument_name = self.class.name.demodulize.downcase
+        ActiveSupport::Notifications.instrument("#{instrument_name}.notifications.travis", :target => self, :args => args, &block)
       end
     end
   end
