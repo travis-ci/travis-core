a27365bd107266398bd255b5016359d88b2fd8b5
Sven Fuchs <svenfuchs@artweb-design.de>
2012-05-12 16:06:41 +0200

    spec Http.builder and Http.data methods

--- spec/travis/api/http/http_spec.rb
+++ spec/travis/api/http/http_spec.rb
@@ -2,9 +2,31 @@ require 'spec_helper'
 require 'travis/api'
 
 describe Travis::Api::Http do
-  describe 'build' do
-    it 'instantiates a builder and returns the data' do
+  describe 'builder' do
+    let(:builder) { Travis::Api::Http.builder(:repository, :version => 'v1') }
 
+    it 'returns the json builder class for the given type and version' do
+      builder.should == Travis::Api::Http::V1::Repository
+    end
+  end
+
+  describe 'data' do
+    let(:repo)    { stub('repository') }
+    let(:builder) { stub('builder', :data => nil) }
+    let(:data)    { Travis::Api::Http::data(:repository, repo, { :foo => 'bar' }, { :version => 'v1' }) }
+
+    before :each do
+      Travis::Api::Http::V1::Repository.stubs(:new).returns(builder)
+    end
+
+    it 'instantiates a builder and returns the data' do
+      Travis::Api::Http::V1::Repository.expects(:new).with(repo, :foo => 'bar').returns(builder)
+      data
+    end
+
+    it 'returns the data from the builder instance' do
+      builder.expects(:data).returns(:data => 'data')
+      data.should == { :data => 'data' }
     end
   end
 end
