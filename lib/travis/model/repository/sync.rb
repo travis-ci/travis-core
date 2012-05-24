a796f89f3c683fc1faa9bd18f4c9d35f7a4698be
Sven Fuchs <svenfuchs@artweb-design.de>
2012-05-24 13:45:25 +0200

    make sure we do not create permissions twice

--- lib/travis/model/repository/sync.rb
+++ lib/travis/model/repository/sync.rb
@@ -1,5 +1,46 @@
 class Repository
   class Sync
+    class Repository
+      attr_reader :user, :data
+
+      def initialize(user, data)
+        @user = user
+        @data = data
+      end
+
+      def run
+        repo = find_or_create
+        repo.update_attributes!(:private => data['private'])
+        permit!(repo) unless permitted?(repo)
+      end
+
+      private
+
+        def find_or_create
+          ::Repository.find_or_create_by_owner_name_and_name(owner_name, name)
+        end
+
+        def permitted?(repo)
+          user.repositories.include?(repo)
+        end
+
+        def permit!(repo)
+          user.permissions.create!(
+            :user => user,
+            :repository => repo,
+            :admin => data['permissions']['admin']
+          )
+        end
+
+        def owner_name
+          data['owner']['login']
+        end
+
+        def name
+          data['name']
+        end
+    end
+
     attr_reader :user
 
     def initialize(user)
@@ -9,23 +50,9 @@ class Repository
     def run
       user.authenticated_on_github do
         Travis::Github.repositories_for(user).each do |data|
-          user.permissions.create!(
-            :user => user,
-            :repository => repo(data),
-            :admin => data['permissions']['admin']
-          )
+          Repository.new(user, data).run
         end
       end
     end
-
-    private
-
-      def repo(data)
-        # This currently does not link the repository to its owner because looking
-        # up the owner type requires an additional request to the github api.
-        Repository.find_or_create_by_owner_name_and_name(data['owner']['login'], data['name']).tap do |repo|
-          repo.update_attributes(:private => data['private'])
-        end
-      end
   end
 end
--- spec/travis/model/repository/sync_spec.rb
+++ spec/travis/model/repository/sync_spec.rb
@@ -8,15 +8,28 @@ describe Repository::Sync do
 
   subject { lambda { Repository::Sync.new(user).run } }
 
+  before :each do
+    GH.stubs(:[]).returns([{ 'name' => 'minimal', 'owner' => { 'login' => 'sven' }, 'permissions' => { 'admin' => true } }])
+  end
+
   it 'fetches all repos the user has access to from the github api' do
     GH.expects(:[]).with('user/repos?per_page=100').returns([])
     subject.call
   end
 
   it 'creates a new repository per record if not yet present' do
-    GH.stubs(:[]).returns([{ 'name' => 'bar', 'owner' => { 'login' => 'foo' }, 'permissions' => { 'admin' => true } }])
     subject.call
-    Repository.find_by_owner_name_and_name('foo', 'bar').should be_present
+    Repository.find_by_owner_name_and_name('sven', 'minimal').should be_present
+  end
+
+  it 'creates a new permission for the user/repo if none exists' do
+    subject.should change(Permission, :count).by(1)
+  end
+
+  it 'does not create a new permission for the user/repo if one exists' do
+    repo = Repository.create(:owner_name => 'sven', :name => 'minimal')
+    user.repositories << repo
+    subject.should_not change(Permission, :count)
   end
 end
 
