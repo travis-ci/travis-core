d67385c1f15e1620c1ba7972915d5cffdfc7bf77
Sven Fuchs <svenfuchs@artweb-design.de>
2012-05-24 15:10:48 +0200

    wrap User::Sync implementation

--- lib/travis/model/user/sync.rb
+++ lib/travis/model/user/sync.rb
@@ -1,15 +1,19 @@
+require 'core_ext/module/include'
+
 class User
   class Sync
-    attr_reader :user
+    include do
+      attr_reader :user
 
-    def initialize(user)
-      @user = user
-    end
+      def initialize(user)
+        @user = user
+      end
 
-    def run
-      Metriks.timer('user.sync').time do
-        Organization.sync_for(user)
-        Repository.sync_for(user)
+      def run
+        Metriks.timer('user.sync').time do
+          Organization.sync_for(user)
+          Repository.sync_for(user)
+        end
       end
     end
   end
