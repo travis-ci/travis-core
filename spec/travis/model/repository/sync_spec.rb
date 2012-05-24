7adf944b1704a69d2e2ee69ebb7f0b5242225894
Sven Fuchs <svenfuchs@artweb-design.de>
2012-05-24 18:42:24 +0200

    add a reverse test for repo creation in repository/sync

--- spec/travis/model/repository/sync_spec.rb
+++ spec/travis/model/repository/sync_spec.rb
@@ -23,6 +23,11 @@ describe Repository::Sync do
     Repository.find_by_owner_name_and_name('sven', 'minimal').should be_present
   end
 
+  it 'does not create a new repository if one exists' do
+    Repository.create!(:owner_name => 'sven', :name => 'minimal')
+    subject.should_not change(Repository, :count)
+  end
+
   it 'creates a new permission for the user/repo if none exists' do
     subject.should change(Permission, :count).by(1)
   end
