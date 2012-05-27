f1bb868b1d7e57b09498a9a5f52c361468bea8ed
Sven Fuchs <svenfuchs@artweb-design.de>
2012-05-27 16:31:45 +0200

    remove stray autoload

--- lib/travis/api/v2.rb
+++ lib/travis/api/v2.rb
@@ -3,7 +3,6 @@ module Travis
     module V2
       autoload :Http,    'travis/api/v2/http'
       autoload :Pusher,  'travis/api/v2/pusher'
-      autoload :Webhook, 'travis/api/v2/webhook'
     end
   end
 end
