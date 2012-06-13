4d9fcc7b668dbf8467b18e1acbb1cc7d2e9b1b80
Sven Fuchs <svenfuchs@artweb-design.de>
2012-06-13 15:06:45 +0200

    add notification build json generator

--- lib/travis/api/v0/notification.rb
+++ lib/travis/api/v0/notification.rb
@@ -2,8 +2,9 @@ module Travis
   module Api
     module V0
       module Notification
-        autoload :User,       'travis/api/v0/notification/user'
+        autoload :Build,      'travis/api/v0/notification/build'
         autoload :Repository, 'travis/api/v0/notification/repository'
+        autoload :User,       'travis/api/v0/notification/user'
       end
     end
   end
