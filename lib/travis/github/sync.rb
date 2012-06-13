4076a68b7ab88d400c397ff53f3c62bc9bb3be85
Sven Fuchs <svenfuchs@artweb-design.de>
2012-06-13 13:00:55 +0200

    remove stray autoload

--- lib/travis/github/sync.rb
+++ lib/travis/github/sync.rb
@@ -3,7 +3,6 @@ module Travis
     module Sync
       autoload :Organizations, 'travis/github/sync/organizations'
       autoload :Repositories,  'travis/github/sync/repositories'
-      autoload :Repository,    'travis/github/sync/repository'
       autoload :User,          'travis/github/sync/user'
     end
   end
