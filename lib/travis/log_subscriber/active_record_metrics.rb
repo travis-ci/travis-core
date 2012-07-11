ce49e4165093a33ee636bee90cb8425cf7275faa
Mathias Meyer <meyer@paperplanes.de>
2012-07-11 22:51:03 +0200

    Add log subscriber to get ActiveRecord instrumentation.

---
+++ lib/travis/log_subscriber/active_record_metrics.rb
@@ -0,0 +1,25 @@
+module Travis
+  module LogSubscriber
+    class ActiveRecordMetrics < ActiveSupport::LogSubscriber
+      def sql(event)
+        return if 'SCHEMA' == event[:name]
+        name, sql, duration = event[:name], event[:sql], event[:duration]
+
+        metric_name =
+          if name.present?
+            Metriks.timer("active_record.reads").update(duration)
+            "active_record.#{name.downcase.gsub(/ /, ".")}"
+          elsif %w{insert delete update}.include?(sql[0..6].downcase)
+            Metriks.timer("active_record.writes").update(duration)
+            "active_record.#{sql[0..6].downcase}"
+          end
+
+        Metriks.timer(metric_name).update(duration)
+      end
+
+      def self.attach
+        attach_to(:active_record)
+      end
+    end
+  end
+end
