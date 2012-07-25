4e68111a389fa63c27bc3833e6277975d30350b7
Sven Fuchs <svenfuchs@artweb-design.de>
2012-07-25 11:45:14 +0200

    do not publish instrumentation data if the event does not get handled

--- lib/travis/notification/instrument/event.rb
+++ lib/travis/notification/instrument/event.rb
@@ -44,17 +44,17 @@ module Travis
 
           class Worker < Handler
             def notify_completed
-              attrs = !job ? {} : {
-                :repository => job.repository.slug,
-                :build => { :id => job.source_id },
-                :job => { :id => job.id, :number => job.number }
-              }
-              publish attrs.merge(
-                :name => object.name,
-                :host => object.host,
-                :queue => handler.queue,
-                :payload => handler.payload
-             )
+              if handler.handle?
+                publish(
+                  :name => object.name,
+                  :host => object.host,
+                  :queue => handler.queue,
+                  :repository => job.repository.slug,
+                  :build => { :id => job.source_id },
+                  :job => { :id => job.id, :number => job.number },
+                  :payload => handler.payload
+               )
+              end
             end
 
             def job
