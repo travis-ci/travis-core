833e8d2cd77e7a97a5c385a1955c2274987cecd4
Sven Fuchs <svenfuchs@artweb-design.de>
2012-08-01 16:41:44 +0200

    add queue-specific metrics and update the readme

--- lib/travis/event/handler/metrics.rb
+++ lib/travis/event/handler/metrics.rb
@@ -13,16 +13,24 @@ module Travis
         def handle
           case event
           when 'job:test:started'
-            meter('job.queue.wait_time', object.created_at, object.started_at)
+            events = %W(job.queue.wait_time job.queue.wait_time.#{queue})
+            meter(events, object.created_at, object.started_at)
           when 'job:test:finished'
-            meter('job.duration', object.started_at, object.finished_at)
+            events = %W(job.duration job.duration.#{queue})
+            meter(events, object.started_at, object.finished_at)
           end
         end
 
         private
 
-          def meter(event, started_at, finished_at)
-            Travis::Instrumentation.meter(event, :started_at => started_at, :finished_at => finished_at)
+          def queue
+            object.queue.gsub('.', '-')
+          end
+
+          def meter(events, started_at, finished_at)
+            events.each do |event|
+              Travis::Instrumentation.meter(event, :started_at => started_at, :finished_at => finished_at)
+            end
           end
       end
     end
