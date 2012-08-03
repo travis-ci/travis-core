7156936e326feb12b8c95026c36e9a76f6e4dacf
Josh Kalderimis <josh.kalderimis@gmail.com>
2012-08-03 13:46:16 +0200

    Since we have better sync where a Permission is only created if the repo is public AND the user has push or admin access, we can now relax the by_member method

--- lib/travis/model/repository.rb
+++ lib/travis/model/repository.rb
@@ -57,7 +57,7 @@ class Repository < ActiveRecord::Base
     end
 
     def by_member(login)
-      administratable.joins(:users).where(:users => { :login => login })
+      joins(:users).where(:users => { :login => login })
     end
 
     def by_slug(slug)
