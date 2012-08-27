a8c2ac84811507dc4930554ffd7eedddb4ea66ea
Sven Fuchs <me@svenfuchs.com>
2012-08-27 16:35:04 +0200

    filter out null chars from logs

--- lib/travis/model/artifact/log.rb
+++ lib/travis/model/artifact/log.rb
@@ -2,8 +2,15 @@ class Artifact::Log < Artifact
   class << self
     # use job_id to avoid loading a log artifact into memory
     def append(id, chars)
-      update_all(["content = COALESCE(content, '') || ?", chars], ["job_id = ?", id])
+      update_all(["content = COALESCE(content, '') || ?", filter(chars)], ["job_id = ?", id])
     end
+
+    private
+
+      def filter(chars)
+        # postgres seems to have issues with null chars
+        chars.gsub("\0", '')
+      end
   end
 
   # def append_message(severity, message)
--- spec/travis/model/artifact/log_spec.rb
+++ spec/travis/model/artifact/log_spec.rb
@@ -5,12 +5,26 @@ describe Artifact::Log do
 
   describe 'class methods' do
     let(:job)   { Factory.create(:test, :log => Factory.create(:log, :content => '')) }
-    let(:lines) { ["line 1\n", "line 2\n", "line 3"] }
+    let(:lines) { ["line 1\n", "line 2\n", 'line 3'] }
 
-    describe ".append" do
-      it "appends streamed build log chunks" do
+    describe 'append' do
+      it 'appends streamed build log chunks' do
         0.upto(2) { |ix| Artifact::Log.append(job.id, lines[ix]) }
         job.reload.log.content.should == lines.join
+      end
+
+      it 'filters out null chars' do
+        Artifact::Log.expects(:update_all).with do |updates, *args|
+          updates.last.should == 'abc'
+        end
+        Artifact::Log.append(job.id, "a\0b\0c")
+      end
+
+      it 'filters out triple null chars' do
+        Artifact::Log.expects(:update_all).with do |updates, *args|
+          updates.last.should == 'abc'
+        end
+        Artifact::Log.append(job.id, "a\000b\000c")
       end
     end
   end
