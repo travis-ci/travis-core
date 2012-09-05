e92dc6d4d36e84d909bcfe46ba0509f266740ad5
Sven Fuchs <me@svenfuchs.com>
2012-09-05 15:07:50 +0200

    add an additional active_record metric for log updates

--- lib/travis/log_subscriber/active_record_metrics.rb
+++ lib/travis/log_subscriber/active_record_metrics.rb
@@ -3,7 +3,7 @@ module Travis
     class ActiveRecordMetrics < ActiveSupport::LogSubscriber
       def sql(event)
         return if 'SCHEMA' == event.payload[:name]
-        name, sql, duration = event.payload[:name], event.payload[:sql], event.duration
+        name, sql, duration = event.payload[:name], event.payload[:sql].downcase, event.duration
         if name.is_a?(Array)
           name = "generic"
         end
@@ -12,12 +12,17 @@ module Travis
           if name.present?
             Metriks.timer("active_record.reads").update(duration)
             "active_record.#{name.downcase.gsub(/ /, ".")}"
-          elsif %w{insert delete update}.include?(sql[0..6].downcase)
+          elsif %w{insert delete update}.include?(sql[0..6])
             Metriks.timer("active_record.writes").update(duration)
-            "active_record.#{sql[0..6].downcase}"
+            Metriks.timer("active_record.log_updates").update(duration) if log_update?(sql)
+            "active_record.#{sql[0..6]}"
           end
 
         Metriks.timer(metric_name).update(duration)
+      end
+
+      def log_update?(sql)
+        sql.include?('artifacts') && sql.include?("content = coalesce(content, '')")
       end
 
       def self.attach
