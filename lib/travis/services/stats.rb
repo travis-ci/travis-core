642ce0421ea33f4a030ef7e303afe6367f612958
Sven Fuchs <me@svenfuchs.com>
2012-09-15 15:10:19 +0200

    use bare sql for stats, can be mapped on the client

--- lib/travis/services/stats.rb
+++ lib/travis/services/stats.rb
@@ -2,42 +2,25 @@ module Travis
   module Services
     class Stats < Base
       def daily_repository_counts
-        total_repos = 0
-        repositories.map do |repo|
-          {
-            :date => repo.created_at,
-            :added_on_date => repo.repos_count.to_i,
-            :total_growth => total_repos += repo.repos_count.to_i
-          }
-        end
+        select scope(:repository).
+          select(['date(created_at) AS date', 'count(created_at) AS count']).
+          where('last_build_id IS NOT NULL').
+          group('created_at').
+          order('created_at').to_sql
       end
 
       def daily_tests_counts
-        tests.map do |job|
-          {
-            :date => job.created_at,
-            :run_on_date => job.config.to_i
-          }
-        end
+        select scope(:job).
+          select(['date(created_at) AS date', 'count(created_at) AS count']).
+          group('created_at').
+          order('created_at').
+          where(['created_at > ?', 28.months.ago]).to_sql
       end
 
       private
 
-        def repositories
-          scope(:repository).
-            select(['date(created_at) AS created_at', 'count(created_at) AS repos_count']).
-            where('last_build_id IS NOT NULL').
-            group('created_at').
-            order('created_at')
-        end
-
-        def tests
-          scope(:job).
-            select(['date(created_at) AS created_at', 'count(created_at) AS config']).
-            group('created_at').
-            order('created_at').
-            where(['created_at > ?', 28.days.ago]).
-            where(['type = ?', 'Job::Test'])
+        def select(sql)
+          ActiveRecord::Base.connection.select_all(sql)
         end
     end
   end
--- spec/travis/services/stats_spec.rb
+++ spec/travis/services/stats_spec.rb
@@ -8,34 +8,26 @@ describe Travis::Services::Stats do
 
   before { Scenario.default }
 
-  describe "when listing daily test counts" do
-    it "should return the jobs per day" do
+  describe 'when listing daily test counts' do
+    it 'should return the jobs per day' do
       stats = service.daily_tests_counts
       stats.should have(1).item
-      stats.first[:date].should == Job.first.created_at.to_date.to_s(:date)
-      stats.first[:run_on_date].should == 13
+      stats.first['date'].should == Job.first.created_at.to_date.to_s(:date)
+      stats.first['count'].should == '13'
     end
   end
 
-  describe "when listing total repositories" do
-    it "should include the date" do
+  describe 'when listing total repositories' do
+    it 'should include the date' do
       stats = service.daily_repository_counts
       stats.should have(1).items
-      stats.first[:date].should == Repository.first.created_at.to_date.to_s(:date)
+      stats.first['date'].should == Repository.first.created_at.to_date.to_s(:date)
     end
 
-    it "should include the number per day" do
+    it 'should include the number per day' do
       stats = service.daily_repository_counts
       stats.should have(1).items
-      stats.first[:added_on_date].should == 2
-    end
-
-    it "should include the total growth" do
-      stats = service.daily_repository_counts
-      stats.should have(1).items
-      stats.first[:total_growth].should == 2
+      stats.first['count'].should == '2'
     end
   end
 end
-
-
