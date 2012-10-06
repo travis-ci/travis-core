d90e2b09c2c33beb62b3be27028878060a33f0e0
Sven Fuchs <me@svenfuchs.com>
2012-10-06 21:20:05 +0200

    add services specs

--- spec/travis/services_spec.rb
+++ spec/travis/services_spec.rb
@@ -3,18 +3,14 @@ require 'spec_helper'
 module Test
   module Services
     module Foo
-      class Base; def initialize(*); end; end
-      class All < Base; end
-      class ByIds < Base; end
-      class One < Base; end
-      class OneOrCreate < Base; end
-      class Update < Base; end
-      class Stuff < Base; end
+      # class Stuff; def initialize(*); end; end
+      class Stuff < Struct.new(:current_user, :params); end
     end
   end
 
   class Foo
     include Travis::Services
+    def current_user; :user; end
   end
 end
 
@@ -32,6 +28,15 @@ describe Travis::Services do
   describe 'service' do
     it 'given a :foo as a type and :stuff as a name it returns an instance of Foo::Stuff' do
       object.service(:foo, :stuff, {}).should be_instance_of(Test::Services::Foo::Stuff)
+    end
+
+    it 'it passes the current user' do
+      object.service(:foo, :stuff, {}).current_user.should == :user
+    end
+
+    it 'it passes the given params' do
+      params = { :some => :thing }
+      object.service(:foo, :stuff, params).params.should == params
     end
   end
 end
