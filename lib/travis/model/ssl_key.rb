6571b8b51a90595179aaa7c684689598c5bae358
Sven Fuchs <me@svenfuchs.com>
2012-11-04 18:46:43 +0100

    move ssl_key encoding to travis-core

--- lib/travis/model/ssl_key.rb
+++ lib/travis/model/ssl_key.rb
@@ -25,6 +25,11 @@ class SslKey < ActiveRecord::Base
     build_key.private_decrypt(string)
   end
 
+  def generate_keys!
+    self.public_key = self.private_key = nil
+    generate_keys
+  end
+
   def generate_keys
     unless public_key && private_key
       keys = OpenSSL::PKey::RSA.generate(1024)
@@ -33,9 +38,13 @@ class SslKey < ActiveRecord::Base
     end
   end
 
-  def generate_keys!
-    self.public_key = self.private_key = nil
-    generate_keys
+  def encoded_public_key
+    key = build_key.public_key
+    ['ssh-rsa ', "\0\0\0\assh-rsa#{sized_bytes(key.e)}#{sized_bytes(key.n)}"].pack('a*m').gsub("\n", '')
+  end
+
+  def encoded_private_key
+    [private_key].pack('m').strip
   end
 
   def secure
@@ -44,7 +53,17 @@ class SslKey < ActiveRecord::Base
 
   private
 
-  def build_key
-    @build_key ||= OpenSSL::PKey::RSA.new(private_key)
-  end
+    def build_key
+      @build_key ||= OpenSSL::PKey::RSA.new(private_key)
+    end
+
+    def sized_bytes(value)
+      bytes = to_byte_array(value.to_i)
+      [bytes.size, *bytes].pack('NC*')
+    end
+
+    def to_byte_array(num, *significant)
+      return significant if num.between?(-1, 0) and significant[0][7] == num[7]
+      to_byte_array(*num.divmod(256)) + significant
+    end
 end
