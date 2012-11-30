0bf960b4166cbbbef2076b442f7cae2c19552f70
Piotr Sarnacki <drogus@gmail.com>
2012-11-30 02:42:43 +0100

    Limit pusher messages for logs to 9kB

--- lib/travis/addons/pusher/task.rb
+++ lib/travis/addons/pusher/task.rb
@@ -4,6 +4,11 @@ module Travis
 
       # Notifies registered clients about various state changes through Pusher.
       class Task < Travis::Task
+
+        def self.chunk_size
+          9 * 1024
+        end
+
         def event
           params[:event]
         end
@@ -34,7 +39,24 @@ module Travis
           def trigger(channel, payload)
             prefix = version == 'v1' ? nil : version
             event = [prefix, client_event].compact.join(':')
-            Travis.pusher[channel].trigger(event, payload)
+            parts(payload).each do |part|
+              Travis.pusher[channel].trigger(event, part)
+            end
+          end
+
+          def parts(payload)
+            if client_event == 'job:log' && payload[:_log].present?
+              # split payload into 9kB chunks, the limit is 10 for entire request
+              # body, 1kB should be enough for headers
+              log = payload[:_log]
+              log.scan(/.{1,#{chunk_size}}/).map { |part| payload.dup.merge(:_log => part) }
+            else
+              [payload]
+            end
+          end
+
+          def chunk_size
+            self.class.chunk_size
           end
 
           Instruments::Task.attach_to(self)
