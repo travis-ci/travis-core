4e5753beb9a93bcf7155b915dc8572a41d8daf87
Piotr Sarnacki <drogus@gmail.com>
2013-01-15 17:06:46 +0100

    Don't remove index on name and host, it's used to order workers

--- db/migrate/20130115145728_cache_full_name_in_workers.rb
+++ db/migrate/20130115145728_cache_full_name_in_workers.rb
@@ -1,7 +1,6 @@
 class CacheFullNameInWorkers < ActiveRecord::Migration
   def change
     add_column :workers, :full_name, :string
-    remove_index :workers, [:name, :host]
     add_index :workers, :full_name
   end
 end
