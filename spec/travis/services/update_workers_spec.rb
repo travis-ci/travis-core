3fa1a75cbba9b5be77e1f0666867215bb41b65c5
Josh Kalderimis <josh.kalderimis@gmail.com>
2013-01-18 02:57:15 +1300

    small spec correction

--- spec/travis/services/update_workers_spec.rb
+++ spec/travis/services/update_workers_spec.rb
@@ -15,7 +15,7 @@ describe Travis::Services::UpdateWorkers do
 
   before :each do
     Worker.create!(:name => 'ruby-1', :host => 'ruby.workers.travis-ci.org', :state => :ready, :last_seen_at => Time.now - 10, :payload => { 'job' => { 'id' => 123 })
-    Worker.create!(:name => 'ruby-2', :host => 'ruby.workers.travis-ci.org', :state => :ready, :last_seen_at => Time.now - 10, :payload => { 'job' => { 'id' => 123 })
+    Worker.create!(:name => 'ruby-2', :host => 'ruby.workers.travis-ci.org', :state => :ready, :last_seen_at => Time.now - 10, :payload => { 'job' => { 'id' => 124 })
     Worker.any_instance.stubs(:notify)
   end
 
