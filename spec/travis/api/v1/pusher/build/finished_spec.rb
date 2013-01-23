871129e3b8002b8c6299ad0dd04e74a7632fc148
Piotr Sarnacki <drogus@gmail.com>
2013-01-23 23:05:47 +0100

    Add description to build:finished pusher event

--- spec/travis/api/v1/pusher/build/finished_spec.rb
+++ spec/travis/api/v1/pusher/build/finished_spec.rb
@@ -25,7 +25,8 @@ describe Travis::Api::V1::Pusher::Build::Finished do
       'last_build_result' => 0,
       'last_build_started_at' => json_format_time(Time.now.utc - 1.minute),
       'last_build_finished_at' => json_format_time(Time.now.utc),
-      'last_build_duration' => 60
+      'last_build_duration' => 60,
+      'description' => 'the repo description',
     }
   end
 end
