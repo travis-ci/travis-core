8433c1248f8c9e247162e8d569a0851cebb7e99c
Piotr Sarnacki <drogus@gmail.com>
2013-05-05 03:46:58 +0200

    Check for branch column on build, not on repository

--- lib/travis/model/repository.rb
+++ lib/travis/model/repository.rb
@@ -105,7 +105,7 @@ class Repository < ActiveRecord::Base
   end
 
   def branches
-    if self.class.column_names.include?('branch')
+    if Build.column_names.include?('branch')
       self.class.connection.select_values %(
         SELECT DISTINCT ON (branch) branch
         FROM   builds
@@ -115,11 +115,11 @@ class Repository < ActiveRecord::Base
       )
     else
       self.class.connection.select_values %(
-        SELECT DISTINCT ON (branch) branch
+        SELECT DISTINCT ON (commits.branch) branch
         FROM   builds
         JOIN   commits ON builds.commit_id = commits.id
         WHERE  builds.repository_id = #{id}
-        ORDER  BY branch DESC
+        ORDER  BY commits.branch DESC
         LIMIT  25
       )
     end
@@ -134,7 +134,7 @@ class Repository < ActiveRecord::Base
   end
 
   def last_finished_builds_by_branches_ids
-    if self.class.column_names.include?('branch')
+    if Build.column_names.include?('branch')
       self.class.connection.select_values %(
         SELECT DISTINCT ON (branch) builds.id
         FROM   builds
@@ -144,11 +144,11 @@ class Repository < ActiveRecord::Base
       )
     else
       self.class.connection.select_values %(
-        SELECT DISTINCT ON (branch) builds.id
+        SELECT DISTINCT ON (commits.branch) builds.id
         FROM   builds
         JOIN   commits ON builds.commit_id = commits.id
         WHERE  builds.repository_id = #{id}
-        ORDER  BY branch, finished_at DESC
+        ORDER  BY commits.branch, finished_at DESC
         LIMIT  25
       )
     end
