7a1392005c7ae501d949a4bb8424187180f5e6a3
Henrik Hodne <me@henrikhodne.com>
2013-05-22 12:26:07 +0200

    Remove status fields from the factories

--- lib/travis/testing/factories.rb
+++ lib/travis/testing/factories.rb
@@ -98,25 +98,22 @@ FactoryGirl.define do
 
   factory :successful_build, :parent => :build do
     repository { |b| Factory(:repository, :name => 'successful_build') }
-    status 0
     state :passed
     started_at { Time.now.utc }
     finished_at { Time.now.utc }
   end
 
   factory :broken_build, :parent => :build do
-    repository { Factory(:repository, :name => 'broken_build', :last_build_status => 1, :last_build_state => :failed) }
-    status 1
+    repository { Factory(:repository, :name => 'broken_build', :last_build_state => :failed) }
     state :failed
     started_at { Time.now.utc }
     finished_at { Time.now.utc }
   end
 
   factory :broken_build_with_tags, :parent => :build do
-    repository  { Factory(:repository, :name => 'broken_build_with_tags', :last_build_status => 1, :last_build_state => :errored) }
+    repository  { Factory(:repository, :name => 'broken_build_with_tags', :last_build_state => :errored) }
     matrix      {[Factory(:test, :tags => "database_missing,rake_not_bundled",   :number => "1.1"),
                   Factory(:test, :tags => "database_missing,log_limit_exceeded", :number => "1.2")]}
-    status      1
     state       :failed
     started_at  { Time.now.utc }
     finished_at { Time.now.utc }
