2a4624aa4175e2baea18382484751afddf22a79a
Henrik Hodne <me@henrikhodne.com>
2013-05-22 12:15:56 +0200

    Mark builds and jobs with NULL results as errored
    Looked through the database and the only jobs/builds matching this are
    jobs from before we added error states, but where the worker would error
    and spew out an exception in the log.
    
    For example: https://travis-ci.org/svenfuchs/globalize3/jobs/849524

--- db/migrate/20130521133050_update_finished_jobs.rb
+++ db/migrate/20130521133050_update_finished_jobs.rb
@@ -1,8 +1,9 @@
 class UpdateFinishedJobs < ActiveRecord::Migration
   def up
-    execute "UPDATE jobs SET state = 'passed' WHERE state = 'finished' AND result = 0"
-    execute "UPDATE jobs SET state = 'failed' WHERE state = 'finished' AND result = 1"
-    count = execute("SELECT COUNT(*) FROM jobs WHERE state = 'finished'").first["count"].to_i
+    execute "UPDATE jobs SET state = 'passed' WHERE state = 'finished' AND result = 0 AND type = 'Job::Test'"
+    execute "UPDATE jobs SET state = 'failed' WHERE state = 'finished' AND result = 1 AND type = 'Job::Test'"
+    execute "UPDATE jobs SET state = 'errored' WHERE state = 'finished' AND result IS NULL AND type = 'Job::Test'"
+    count = execute("SELECT COUNT(*) FROM jobs WHERE state = 'finished' AND type = 'Job::Test'").first["count"].to_i
     raise "Finished jobs remaining" unless count == 0
   end
 
