5a008d0f79fb0efeda63f6cf8ccd1db0c736da38
Henrik Hodne <me@henrikhodne.com>
2013-07-04 12:29:46 -0700

    Convert non-breaking spaces in .travis.yml to normal spaces

--- lib/travis/github/services/fetch_config.rb
+++ lib/travis/github/services/fetch_config.rb
@@ -37,6 +37,8 @@ module Travis
             Travis.logger.warn("[request:fetch_config] Empty content for #{config_url}") if content.nil?
             content = content.to_s.unpack('m').first
             Travis.logger.warn("[request:fetch_config] Empty unpacked content for #{config_url}, content was #{content.inspect}") if content.nil?
+            content = content.gsub(/^(\xC2\xA0)+/) { |match| match.gsub("\xC2\xA0", " ") }
+
             content
           end
 
