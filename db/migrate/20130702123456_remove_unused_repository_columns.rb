aee93fa09b97ee50ceb1af5a853db766c98e8deb
Josh Kalderimis <josh.kalderimis@gmail.com>
2013-07-05 19:06:47 +0200

    oops, :repositories not :repository in the migrations

--- db/migrate/20130702123456_remove_unused_repository_columns.rb
+++ db/migrate/20130702123456_remove_unused_repository_columns.rb
@@ -1,8 +1,8 @@
 class RemoveUnusedRepositoryColumns < ActiveRecord::Migration
   def change
-    remove_column :repository, :last_duration
-    remove_column :repository, :last_build_status
-    remove_column :repository, :last_build_result
-    remove_column :repository, :last_build_language
+    remove_column :repositories, :last_duration
+    remove_column :repositories, :last_build_status
+    remove_column :repositories, :last_build_result
+    remove_column :repositories, :last_build_language
   end
 end
