73eda47b3eeb2ec15fc6d9d6bca5f465f6297570
Josh Kalderimis <josh.kalderimis@gmail.com>
2013-07-05 15:41:03 +0200

    fix some payload tests from the last_build_language changes

--- spec/travis/api/v1/http/repositories_spec.rb
+++ spec/travis/api/v1/http/repositories_spec.rb
@@ -14,8 +14,8 @@ describe Travis::Api::V1::Http::Repositories do
       'last_build_number' => repository.last_build_number,
       'last_build_started_at' => json_format_time(Time.now.utc - 1.minute),
       'last_build_finished_at' => json_format_time(Time.now.utc),
-      'last_build_status' => repository.last_build_result, # still here for backwards compatibility
-      'last_build_result' => repository.last_build_result,
+      'last_build_status' => 0, # still here for backwards compatibility
+      'last_build_result' => 0,
       'last_build_language' => 'ruby',
       'last_build_duration' => 60
     }
