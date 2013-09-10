2d57f6bdd87c0d4c61e56c35b3b5d4ebaa4bf4cd
Piotr Sarnacki <drogus@gmail.com>
2013-09-10 12:38:58 +0200

    Remove updated_at from FindBuilds service
    The query to fetch it can take a lot of the time, which a lot of the
    time is slower than actually getting a list of builds.

--- spec/travis/services/find_builds_spec.rb
+++ spec/travis/services/find_builds_spec.rb
@@ -62,14 +62,4 @@ describe Travis::Services::FindBuilds do
       end
     end
   end
-
-  describe 'updated_at' do
-    it 'returns the latest updated_at time' do
-      @params = { :repository_id => repo.id }
-      Build.delete_all
-      Factory(:build, :repository => repo, :state => :finished, :number => 1, :updated_at => Time.now - 1.hour)
-      Factory(:build, :repository => repo, :state => :finished, :number => 1, :updated_at => Time.now)
-      service.updated_at.to_s.should == Time.now.to_s
-    end
-  end
 end
