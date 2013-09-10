23ec3baee82f55e17f58d2b885933f316ee4e8f1
Piotr Sarnacki <drogus@gmail.com>
2013-09-10 20:52:16 +0200

    Add index on builds(id DESC, repository_id, event_type)

---
+++ db/migrate/20130910184823_create_index_on_builds_id_desc_and_repository_id_and_event_type.rb
@@ -0,0 +1,9 @@
+class CreateIndexOnBuildsIdDescAndRepositoryIdAndEventType < ActiveRecord::Migration
+  def up
+    execute "CREATE INDEX CONCURRENTLY index_builds_on_id_repository_id_and_event_type_desc ON builds (id DESC, repository_id, event_type);"
+  end
+
+  def down
+    execute "DROP INDEX index_builds_on_id_repository_id_and_event_type_desc;"
+  end
+end
