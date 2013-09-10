b13bd0e7b8de2b105582dfb669bc097d8181c2a4
Piotr Sarnacki <drogus@gmail.com>
2013-09-10 20:59:53 +0200

    Disable ddl transaction for index added concurrently

--- db/migrate/20130910184823_create_index_on_builds_id_desc_and_repository_id_and_event_type.rb
+++ db/migrate/20130910184823_create_index_on_builds_id_desc_and_repository_id_and_event_type.rb
@@ -1,4 +1,6 @@
 class CreateIndexOnBuildsIdDescAndRepositoryIdAndEventType < ActiveRecord::Migration
+  self.disable_ddl_transaction!
+
   def up
     execute "CREATE INDEX CONCURRENTLY index_builds_on_id_repository_id_and_event_type_desc ON builds (id DESC, repository_id, event_type);"
   end
