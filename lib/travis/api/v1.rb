59b7b7c26c1911de7ba36136e820c45afd832049
Josh Kalderimis <josh.kalderimis@gmail.com>
2013-09-16 14:48:57 +0200

    removed unused job and build archive api payloads

--- lib/travis/api/v1.rb
+++ lib/travis/api/v1.rb
@@ -1,7 +1,6 @@
 module Travis
   module Api
     module V1
-      autoload :Archive, 'travis/api/v1/archive'
       autoload :Http,    'travis/api/v1/http'
       autoload :Helpers, 'travis/api/v1/helpers'
       autoload :Webhook, 'travis/api/v1/webhook'
