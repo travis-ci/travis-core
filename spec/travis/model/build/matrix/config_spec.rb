27db0e9f532f63b7625a5878ce29922ace0f39af
Hiro Asari <asari.ruby@gmail.com>
2013-12-13 23:32:39 -0500

    Fix indentation [skip ci]

--- spec/travis/model/build/matrix/config_spec.rb
+++ spec/travis/model/build/matrix/config_spec.rb
@@ -25,24 +25,24 @@ class Build
         config.exclude_config?({})
       end
 
-     context 'multi_os feature is active' do
-       before :each do
-         repo    = Factory(:repository)
-         request = Factory(:request, repository: repo)
-         build   = Factory(:build, config: matrix_with_os_ruby, request: request)
-         Travis::Features.stubs(:active?).with(:multi_os, repo).returns(true)
-         @config = Config.new(build)
-       end
+      context 'multi_os feature is active' do
+        before :each do
+          repo    = Factory(:repository)
+          request = Factory(:request, repository: repo)
+          build   = Factory(:build, config: matrix_with_os_ruby, request: request)
+          Travis::Features.stubs(:active?).with(:multi_os, repo).returns(true)
+          @config = Config.new(build)
+        end
 
-       it 'expands on :os' do
-         @config.expand.should == [
-           [[:os, 'osx'], [:rvm, '2.0.0'], [:gemfile, 'gemfiles/rails-4']],
-           [[:os, 'osx'], [:rvm, '1.9.3'], [:gemfile, 'gemfiles/rails-4']],
-           [[:os, 'linux'], [:rvm, '2.0.0'], [:gemfile, 'gemfiles/rails-4']],
-           [[:os, 'linux'], [:rvm, '1.9.3'], [:gemfile, 'gemfiles/rails-4']]
-         ]
-       end
-     end
+        it 'expands on :os' do
+          @config.expand.should == [
+            [[:os, 'osx'], [:rvm, '2.0.0'], [:gemfile, 'gemfiles/rails-4']],
+            [[:os, 'osx'], [:rvm, '1.9.3'], [:gemfile, 'gemfiles/rails-4']],
+            [[:os, 'linux'], [:rvm, '2.0.0'], [:gemfile, 'gemfiles/rails-4']],
+            [[:os, 'linux'], [:rvm, '1.9.3'], [:gemfile, 'gemfiles/rails-4']]
+          ]
+        end
+      end
 
       context 'multi_os feature is inactive' do
         before :each do
