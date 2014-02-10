4dfcd4359766a806a833d80b25058e8a25d54139
Henrik Hodne <henrik@hodne.io>
2014-02-10 00:53:26 -0600

    fix(db): add newlines to membership-indices migration
    My editor got borked and inserted carriage returns instead of newlines.

--- db/migrate/20140210003014_add_indices_to_memberships.rb
+++ db/migrate/20140210003014_add_indices_to_memberships.rb
@@ -2,8 +2,10 @@ class AddIndicesToMemberships < ActiveRecord::Migration
   self.disable_ddl_transaction!
 
   def up
-    execute "CREATE INDEX CONCURRENTLY index_memberships_on_user_id ON memberships(user_id)"  end
+    execute "CREATE INDEX CONCURRENTLY index_memberships_on_user_id ON memberships(user_id)"
+  end
 
   def down
-    execute "DROP INDEX CONCURRENTLY index_memberships_on_user_id"  end
+    execute "DROP INDEX CONCURRENTLY index_memberships_on_user_id"
+  end
 end
