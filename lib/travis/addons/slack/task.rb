2d4a7625c689094b5061d38a2dcbd48f7cd60f1d
Mathias Meyer <meyer@paperplanes.de>
2014-03-11 17:25:03 +0100

    Improved Slack notification message.

--- lib/travis/addons/slack/task.rb
+++ lib/travis/addons/slack/task.rb
@@ -42,7 +42,7 @@ module Travis
         end
 
         def message_text
-          line = "[travis-ci] Build #%{build_number} (<%{compare_url}|Changes>) by %{author} <%{build_url}|%{result}>"
+          line = "[travis-ci] Build #%{build_number} (<%{compare_url}|%{commit}>) of %{repository}@%{branch} by %{author} <%{build_url}|%{result}>"
           Util::Template.new(line, payload).interpolate
         end
 
