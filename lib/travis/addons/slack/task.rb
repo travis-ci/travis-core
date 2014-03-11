101af900e1f96217f6843d7af6973c73100fa5d3
Mathias Meyer <meyer@paperplanes.de>
2014-03-11 18:16:24 +0100

    Include a custom icon in the Slack message.

--- lib/travis/addons/slack/task.rb
+++ lib/travis/addons/slack/task.rb
@@ -38,12 +38,24 @@ module Travis
             message[:channel] = "#{channel}"
           end
 
+          add_custom_image(message)
+
           message
         end
 
         def message_text
           line = "[travis-ci] Build #%{build_number} (<%{compare_url}|%{commit}>) of %{repository}@%{branch} by %{author} <%{build_url}|%{result}> in %{duration}"
           Util::Template.new(line, payload).interpolate
+        end
+
+        def add_custom_image(message)
+          image = case payload[:build][:state].to_s
+                  when 'passed'
+                    'mascot-passed.png'
+                  when 'failed'
+                    'mascot-failed.png'
+                  end
+          message[:icon_url] = "http://paperplanes-assets.s3.amazonaws.com/#{image}"
         end
 
         Instruments::Task.attach_to(self)
