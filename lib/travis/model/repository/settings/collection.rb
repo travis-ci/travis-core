7cf84e2d74fe933abf147e2b4adc02aba8b9d874
Piotr Sarnacki <drogus@gmail.com>
2014-03-24 14:01:22 +0100

    Move classes from Repository::Settings to separate files

---
+++ lib/travis/model/repository/settings/collection.rb
@@ -0,0 +1,52 @@
+module Travis
+  class Settings
+    class Collection
+      include Enumerable
+
+      class << self
+        def model(model_name_or_class = nil)
+          if model_name_or_class
+            klass = if model_name_or_class.is_a?(String) || model_name_or_class.is_a?(Symbol)
+              name = model_name_or_class.to_s.classify
+              Repository::Settings.const_get(name)
+            else
+              model_name_or_class
+            end
+
+            @model_class = klass
+          else
+            @model_class
+          end
+        end
+        attr_reader :model_class
+      end
+
+      attr_reader :collection
+      attr_accessor :registered_at
+      delegate :model_class, to: 'self.class'
+      delegate :each, :length, :empty?, to: :collection
+
+      def initialize
+        @collection = []
+      end
+
+      def create(attributes)
+        model = model_class.new(attributes)
+        model.id = SecureRandom.uuid unless model.id
+        collection.push model
+        model
+      end
+
+      def load(array)
+        array.each do |attributes|
+          model = model_class.new(attributes)
+          collection.push model
+        end
+      end
+
+      def to_hashes
+        collection.map(&:to_hash)
+      end
+    end
+  end
+end
---
+++ lib/travis/model/repository/settings/field.rb
@@ -0,0 +1,57 @@
+module Travis
+  class Settings
+    class Errors < ActiveModel::Errors
+      # Default behavior of Errors in Active Model is to
+      # translate symbolized message into full text message,
+      # using i18n if available. I don't want such a behavior,
+      # as I want to return error "codes" like :blank, not
+      # full text like "can't be blank"
+      def normalize_message(attribute, message, options)
+        message || :invalid
+      end
+    end
+
+    class Field < Struct.new(:name, :type, :options)
+      attr_reader :encrypted
+      alias encrypted? encrypted
+
+      def initialize(name, type, options)
+        super
+        @encrypted = true if options[:encrypted] || options['encrypted']
+      end
+
+      def set(value, options)
+        encrypt(coerce(value), options[:key])
+      end
+
+      def get(value, options)
+        wrap_as_encrypted(value, options[:key])
+      end
+
+      def coerce(value)
+        coercer = Coercible::Coercer.new
+        coercer[value.class].send(coercer_method, value)
+      end
+
+      def coercer_method
+        if type == :uuid
+          'to_string'
+        else
+          "to_#{type}"
+        end
+      end
+
+      def wrap_as_encrypted(value, key)
+        encrypted? ? EncryptedValue.new(value, key) : value
+      end
+
+      def encrypt(value, key)
+        if encrypted?
+          Travis::Model::EncryptedColumn.new(key: key, use_prefix: false).dump(value)
+        else
+          value
+        end
+      end
+    end
+  end
+end
