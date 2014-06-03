74c6111b4fee0f426c1032b32f4e54ad7a490fe7
Piotr Sarnacki <drogus@gmail.com>
2014-06-03 12:26:17 +0200

    Add even more logging for states cache event handler

--- lib/travis/addons/states_cache/event_handler.rb
+++ lib/travis/addons/states_cache/event_handler.rb
@@ -5,17 +5,29 @@ module Travis
         EVENTS = /build:finished/
 
         def handle?
-          !pull_request? && Travis::Features.feature_active?(:states_cache)
+          result = !pull_request? && Travis::Features.feature_active?(:states_cache)
+          Travis.logger.info("[states-cache] Checking if event handler should be run for repo_id=#{repository_id} branch=#{branch}, result: #{result}")
+          result
         end
 
         def handle
-          branch = commit['branch']
-          repository_id = build['repository_id']
+          Travis.logger.info("[states-cache] Running event handler for repo_id=#{repository_id} branch=#{branch}")
           cache.write(repository_id, branch, data)
+        rescue Exception => e
+          Travis.logger.error("[states-cache] An error occurred while trying to handle states cache update: #{e.message}\n#{e.backtrace}")
+          raise
         end
 
         def cache
           Travis.states_cache
+        end
+
+        def repository_id
+          build['repository_id']
+        end
+
+        def branch
+          commit['branch']
         end
 
         def data
