7fa71f8fa67df23dc90b6bba64a82e7286e67064
Hiro Asari <asari.ruby@gmail.com>
2014-06-10 11:20:12 -0400

    Use JRuby-1.7.12 for travis-logs

--- set_up_travis_logs.sh
+++ set_up_travis_logs.sh
@@ -5,10 +5,13 @@ pushd $HOME
 git clone --depth=1 https://github.com/travis-ci/travis-logs.git
 cd travis-logs
 
+# install jruby-1.7.12 until it is preinstalled on the worker
+JRUBY_VERSION=1.7.12
+rvm install jruby-$JRUBY_VERSION
 # using JRuby, migrate the 'logs' table in 'travis_test' database
 BUNDLE_GEMFILE=$PWD/Gemfile
-rvm jruby-19mode do bundle install
+rvm jruby-$JRUBY_VERSION do bundle install
 psql -c "CREATE DATABASE travis_logs_test;" -U postgres
 cp $TRAVIS_BUILD_DIR/config/database.yml config/travis.yml
-rvm jruby-19mode do bundle exec rake db:migrate
+rvm jruby-$JRUBY_VERSION do bundle exec rake db:migrate
 popd
