a4f5fc05e7bbedaff3ddcab9aa025e06da157026
Hiro Asari <asari.ruby@gmail.com>
2014-06-10 14:46:00 -0400

    Read .ruby-version to choose Ruby runtime for logs

--- set_up_travis_logs.sh
+++ set_up_travis_logs.sh
@@ -5,13 +5,13 @@ pushd $HOME
 git clone --depth=1 https://github.com/travis-ci/travis-logs.git
 cd travis-logs
 
-# install jruby-1.7.12 until it is preinstalled on the worker
-JRUBY_VERSION=1.7.12
-rvm install jruby-$JRUBY_VERSION
+# install ruby runtime which travis-logs wants
+RUBY_RUNTIME=$(cat .ruby-version)
+rvm install $RUBY_RUNTIME
 # using JRuby, migrate the 'logs' table in 'travis_test' database
 BUNDLE_GEMFILE=$PWD/Gemfile
-rvm jruby-$JRUBY_VERSION do bundle install
+rvm $RUBY_RUNTIME do bundle install
 psql -c "CREATE DATABASE travis_logs_test;" -U postgres
 cp $TRAVIS_BUILD_DIR/config/database.yml config/travis.yml
-rvm jruby-$JRUBY_VERSION do bundle exec rake db:migrate
+rvm $RUBY_RUNTIME do bundle exec rake db:migrate
 popd
