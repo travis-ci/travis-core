4a43535337a322b5c154c0ac478f038b5cb13132
Hiro Asari <asari.ruby@gmail.com>
2014-06-10 10:49:48 -0400

    Clean up set_up_travis_logs.sh
    Remove superfluous parts

--- set_up_travis_logs.sh
+++ set_up_travis_logs.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-OLD_BUNDLE_GEMFILE=$BUNDLE_GEMFILE
-
 # clone travis-logs
 pushd $HOME
 git clone --depth=1 https://github.com/travis-ci/travis-logs.git
 cd travis-logs
 
 # using JRuby, migrate the 'logs' table in 'travis_test' database
-export BUNDLE_GEMFILE=$PWD/Gemfile
+BUNDLE_GEMFILE=$PWD/Gemfile
 rvm jruby-19mode do bundle install
 psql -c "CREATE DATABASE travis_logs_test;" -U postgres
 cp $TRAVIS_BUILD_DIR/config/database.yml config/travis.yml
 rvm jruby-19mode do bundle exec rake db:migrate
 popd
-rvm $TRAVIS_RUBY_VERSION
-export BUNDLE_GEMFILE=$OLD_BUNDLE_GEMFILE
