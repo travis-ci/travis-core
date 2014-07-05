25fb0cac632ddba0e2e0f55d6a62b20d402264a4
Hiro Asari <asari.ruby@gmail.com>
2014-07-05 23:53:26 +0200

    Use Log#clear! before adding log removal data

--- lib/travis/services/remove_log.rb
+++ lib/travis/services/remove_log.rb
@@ -33,10 +33,9 @@ module Travis
           message << "\n\n#{params[:reason]}"
         end
 
+        log.clear!
         log.update_attributes(
           :content => message,
-          :archive_verified => false,
-          :archived_at => nil,
           :removed_at => removed_at,
           :removed_by => current_user
         )
