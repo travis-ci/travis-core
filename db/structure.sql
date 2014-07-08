070166e1b6469824ffb89eafc6e8f7f89464156d
Piotr Sarnacki <drogus@gmail.com>
2014-07-08 13:47:49 +0200

    Updte structure.sql

--- db/structure.sql
+++ db/structure.sql
@@ -3,7 +3,6 @@
 --
 
 SET statement_timeout = 0;
-SET lock_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
@@ -453,7 +452,9 @@ CREATE TABLE logs (
     archiving boolean,
     archived_at timestamp without time zone,
     archive_verified boolean,
-    purged_at timestamp without time zone
+    purged_at timestamp without time zone,
+    removed_at timestamp without time zone,
+    removed_by integer
 );
 
 
@@ -1554,3 +1555,5 @@ INSERT INTO schema_migrations (version) VALUES ('20140204220926');
 INSERT INTO schema_migrations (version) VALUES ('20140210003014');
 
 INSERT INTO schema_migrations (version) VALUES ('20140210012509');
+
+INSERT INTO schema_migrations (version) VALUES ('20140612131826');
