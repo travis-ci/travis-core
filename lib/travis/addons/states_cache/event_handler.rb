9d8e82f9f88a188aca43a5fdfe7d8cb67175bbc8
Piotr Sarnacki <drogus@gmail.com>
2014-08-01 12:06:10 +0200

    More debugging for states cache addon

--- lib/travis/addons/states_cache/event_handler.rb
+++ lib/travis/addons/states_cache/event_handler.rb
@@ -5,13 +5,16 @@ module Travis
         EVENTS = /build:finished/
 
         def handle?
-          result = !pull_request? && Travis::Features.feature_active?(:states_cache)
-          Travis.logger.info("[states-cache] Checking if event handler should be run for repo_id=#{repository_id} branch=#{branch}, result: #{result}")
+          states_cache_enabled = Travis::Features.feature_active?(:states_cache)
+          result = !pull_request? && states_cache_enabled
+          Travis.logger.info("[states-cache] Checking if event handler should be run for " +
+            "repo_id=#{repository_id} branch=#{branch} build_id=#{build['id']}, result: #{result}, " +
+            "pull_request: #{pull_request?} states_cache_enabled: #{states_cache_enabled}")
           result
         end
 
         def handle
-          Travis.logger.info("[states-cache] Running event handler for repo_id=#{repository_id} branch=#{branch}")
+          Travis.logger.info("[states-cache] Running event handler for repo_id=#{repository_id} build_id=#{build['id']} branch=#{branch}")
           cache.write(repository_id, branch, data)
         rescue Exception => e
           Travis.logger.error("[states-cache] An error occurred while trying to handle states cache update: #{e.message}\n#{e.backtrace}")
