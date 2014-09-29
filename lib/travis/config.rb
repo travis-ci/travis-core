168010b4cea4153cd8252b4da5355fabe7e59a2d
Sven Fuchs <me@svenfuchs.com>
2014-09-29 15:50:38 +0200

    convert config to ruby 2 hash syntax, add setting max timeouts

--- lib/travis/config.rb
+++ lib/travis/config.rb
@@ -78,43 +78,43 @@ module Travis
     end
 
     HOSTS = {
-      :production  => 'travis-ci.org',
-      :staging     => 'staging.travis-ci.org',
-      :development => 'localhost:3000'
+      production:  'travis-ci.org',
+      staging:     'staging.travis-ci.org',
+      development: 'localhost:3000'
     }
 
     include Logging
 
-    define  :host          => 'travis-ci.org',
-            :shorten_host  => 'trvs.io',
-            :tokens        => { :internal => 'token' },
-            :assets        => { :host => HOSTS[Travis.env.to_sym], :version => defined?(Travis::Assets) ? Travis::Assets.version : 'asset-id', :interval => 15 },
-            :amqp          => { :username => 'guest', :password => 'guest', :host => 'localhost', :prefetch => 1 },
-            :database      => { :adapter => 'postgresql', :database => "travis_#{Travis.env}", :encoding => 'unicode', :min_messages => 'warning' },
-            :s3            => { :access_key_id => '', :secret_access_key => '' },
-            :pusher        => { :app_id => 'app-id', :key => 'key', :secret => 'secret' },
-            :sidekiq       => { :namespace => 'sidekiq', :pool_size => 1 },
-            :smtp          => {},
-            :github        => { :api_url => 'https://api.github.com', :token => 'travisbot-token' },
-            :async         => {},
-            :notifications => [], # TODO rename to event.handlers
-            :queues        => [],
-            :default_queue => 'builds.linux',
-            :workers       => { :ttl => 60, :prune => { :interval => 5 } },
-            :jobs          => { :retry => { :after => 60 * 60 * 2, :max_attempts => 1, :interval => 60 * 5 } },
-            :queue         => { :limit => { :default => 5, :by_owner => {} }, :interval => 3 },
-            :logs          => { :shards => 1, :intervals => { :vacuum => 10, :regular => 180, :force => 3 * 60 * 60 } },
-            :email         => {},
-            :roles         => {},
-            :archive       => {},
-            :ssl           => {},
-            :redis         => { :url => 'redis://localhost:6379' },
-            :repository_filter => { :include => [/^rails\/rails/], :exclude => [/\/rails$/] },
-            :encryption    => (Travis.env == 'development' || Travis.env == 'test' ? { key: 'secret'*10 } : {}),
-            :sync          => { :organizations => { :repositories_limit => 1000 } },
-            :states_cache  => { :memcached_servers => 'localhost:11211' },
-            :sentry        => { },
-            :services      => { :find_requests => { :max_limit => 100, :default_limit => 25 } }
+    define  host:  'travis-ci.org',
+            shorten_host:  'trvs.io',
+            tokens:        { internal: 'token' },
+            assets:        { host: HOSTS[Travis.env.to_sym], version: defined?(Travis::Assets) ? Travis::Assets.version : 'asset-id', interval: 15 },
+            amqp:          { username: 'guest', password: 'guest', host: 'localhost', prefetch: 1 },
+            database:      { adapter: 'postgresql', database: "travis_#{Travis.env}", encoding: 'unicode', min_messages: 'warning' },
+            s3:            { access_key_id: '', secret_access_key: '' },
+            pusher:        { app_id: 'app-id', key: 'key', secret: 'secret' },
+            sidekiq:       { namespace: 'sidekiq', pool_size: 1 },
+            smtp:          {},
+            github:        { api_url: 'https://api.github.com', token: 'travisbot-token' },
+            async:         {},
+            notifications: [], # TODO rename to event.handlers
+            queues:        [],
+            default_queue: 'builds.linux',
+            jobs:          { retry: { after: 60 * 60 * 2, max_attempts: 1, interval: 60 * 5 } },
+            queue:         { limit: { default: 5, by_owner: {} }, interval: 3 },
+            logs:          { shards: 1, intervals: { vacuum: 10, regular: 180, force: 3 * 60 * 60 } },
+            email:         {},
+            roles:         {},
+            archive:       {},
+            ssl:           {},
+            redis:         { url: 'redis://localhost:6379' },
+            repository_filter: { include: [/^rails\/rails/], exclude: [/\/rails$/] },
+            encryption:    Travis.env == 'development' || Travis.env == 'test' ? { key: 'secret'*10 } : {},
+            sync:          { organizations: { repositories_limit: 1000 } },
+            states_cache:  { memcached_servers: 'localhost:11211' },
+            sentry:        { },
+            services:      { find_requests: { max_limit: 100, default_limit: 25 } },
+            settings:      { timeouts: { defaults: { hard_limit: 50, log_silence: 10 }, maximums: { hard_limit: 180, log_silence: 60 } } }
 
     default :_access => [:key]
 
