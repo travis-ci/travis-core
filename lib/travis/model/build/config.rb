344be527b896b0592c09f2532d24b86e7792eb86
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2015-01-08 15:57:32 +0100

    add Dart language key and expansion

--- lib/travis/model/build/config.rb
+++ lib/travis/model/build/config.rb
@@ -12,7 +12,7 @@ class Build
 
     DEFAULT_LANG = 'ruby'
 
-    ENV_KEYS = [:rvm, :gemfile, :env, :otp_release, :php, :node_js, :scala, :jdk, :python, :perl, :compiler, :go, :xcode_sdk, :xcode_scheme, :ghc, :ruby, :rust, :csharp, :fsharp, :visualbasic, :d, :julia]
+    ENV_KEYS = [:rvm, :gemfile, :env, :otp_release, :php, :node_js, :scala, :jdk, :python, :perl, :compiler, :go, :xcode_sdk, :xcode_scheme, :ghc, :ruby, :rust, :csharp, :fsharp, :visualbasic, :d, :julia, :dart]
 
     EXPANSION_KEYS_FEATURE = [:os]
 
@@ -23,6 +23,7 @@ class Build
       'cpp'         => [:compiler],
       'csharp'      => [:csharp],
       'd'           => [:d],
+      'dart'        => [:dart],
       'erlang'      => [:otp_release],
       'fsharp'      => [:fsharp],
       'go'          => [:go],
