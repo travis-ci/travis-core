e3b04d68c1a74e3dc7a70df178fee489c22ac61a
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2015-02-23 13:13:21 +0100

    add branches table

---
+++ db/migrate/20150223125700_create_branches.rb
@@ -0,0 +1,15 @@
+class CreateBranches < ActiveRecord::Migration
+  def up
+    create_table(:branches) do |t|
+      t.integer :repository_id, null: false
+      t.integer :last_build_id, null: false
+      t.string  :name
+      t.timestamps
+    end
+    add_index(:branches, [:repository_id, :name])
+  end
+
+  def down
+    drop_table(:branches)
+  end
+end
