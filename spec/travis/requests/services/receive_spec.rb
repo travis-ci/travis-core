0513832933645b5b0706c10820e9511b4ffdf919
Josh Kalderimis <josh.kalderimis@gmail.com>
2015-03-12 23:14:50 +1300

    change back to id vs github_id
    that is what we use internally

--- spec/travis/requests/services/receive_spec.rb
+++ spec/travis/requests/services/receive_spec.rb
@@ -111,12 +111,12 @@ describe Travis::Requests::Services::Receive, truncation: true do
     let!(:repo)   { Factory(:repository, github_id: 592533, owner: owner, owner_name: owner.login, name: 'gem-release') }
 
     describe 'giving the repo owner_id' do
-      let(:payload) { { 'repository' => { 'owner_id' => owner.id, 'owner_type' => 'User', 'owner_name' => 'svenfuchs', 'name' => 'gem-release', 'github_id' => 111 }, 'user' => { 'id' => 1 } } }
+      let(:payload) { { 'repository' => { 'owner_id' => owner.id, 'owner_type' => 'User', 'owner_name' => 'svenfuchs', 'name' => 'gem-release', 'id' => 111 }, 'user' => { 'id' => 1 } } }
       it_should_behave_like 'creates a request'
     end
 
     describe 'giving the repo owner_name' do
-      let(:payload) { { 'repository' => { 'owner_name' => 'svenfuchs', 'name' => 'gem-release', 'github_id' => 111 }, 'user' => { 'id' => 1 } } }
+      let(:payload) { { 'repository' => { 'owner_name' => 'svenfuchs', 'name' => 'gem-release', 'id' => 111 }, 'user' => { 'id' => 1 } } }
       it_should_behave_like 'creates a request'
     end
   end
