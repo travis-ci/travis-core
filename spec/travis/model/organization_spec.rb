613dcb06b0b053135efeb0d565f1ecc6fc9e0dab
carlad <carla@travis-ci.com>
2015-04-20 17:25:09 +0200

    fix the test

--- spec/travis/model/organization_spec.rb
+++ spec/travis/model/organization_spec.rb
@@ -4,17 +4,19 @@ describe User do
   include Support::ActiveRecord
 
     let(:org)    { Factory.create(:org, :login => 'travis-organization') }
-    let(:org2)         { Factory.create(:org, :login => 'travis-ci') }
 
     describe 'educational_org' do
+      after do
+        Travis::Features.deactivate_owner(:educational_org, org)
+      end
+
       it 'returns true if organization is flagged as educational_org' do
         Travis::Features.activate_owner(:educational_org, org)
-        Travis::Features.owner_active?(:educational_org, org).should be_true
+        org.education?.should be_true
       end
 
       it 'returns false if the organization has not been flagged as educational_org' do
-        Travis::Features.activate_owner(:educational_org, org)
-        Travis::Features.owner_active?(:educational_org, org2).should be_false
+        org.education?.should be_false
       end
     end
 end
