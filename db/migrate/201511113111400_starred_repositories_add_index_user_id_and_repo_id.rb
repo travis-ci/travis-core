b4ef238a9e98c49099f894e35e1f15dab1a1e5f0
carlad <carla@travis-ci.com>
2015-11-13 11:22:32 +0100

    add index for user and repo ids on starred_repos

---
+++ db/migrate/201511113111400_starred_repositories_add_index_user_id_and_repo_id.rb
@@ -0,0 +1,11 @@
+class StarredRepositoriesAddIndexUserIdAndRepoId < ActiveRecord::Migration
+  self.disable_ddl_transaction!
+
+  def up
+    execute "CREATE INDEX index_starred_repositories_on_user_id_and_repo_id ON starred_repositories (user_id, repo_id)"
+  end
+
+  def down
+    execute "DELETE INDEX index_starred_repositories_on_user_id_and_repo_id"
+  end
+end
