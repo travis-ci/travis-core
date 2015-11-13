6f4bf47378f411cb9a3b796fee4447c53ff2ffc3
carlad <carla@travis-ci.com>
2015-11-13 11:44:50 +0100

    update index migration

--- db/migrate/201511113111400_starred_repositories_add_index_user_id_and_repo_id.rb
+++ db/migrate/201511113111400_starred_repositories_add_index_user_id_and_repo_id.rb
@@ -2,7 +2,7 @@ class StarredRepositoriesAddIndexUserIdAndRepoId < ActiveRecord::Migration
   self.disable_ddl_transaction!
 
   def up
-    execute "CREATE INDEX index_starred_repositories_on_user_id_and_repo_id ON starred_repositories (user_id, repo_id)"
+    execute "CREATE UNIQUE INDEX index_starred_repositories_on_user_id_and_repo_id ON starred_repositories (user_id, repo_id)"
   end
 
   def down
